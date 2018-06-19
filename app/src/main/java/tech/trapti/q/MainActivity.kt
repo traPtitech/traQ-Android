6f758a8e3de97b7c3fc748ffb341cdbdc76a204f
とーふとふ <tohu.soy@gmail.com>
2018-06-20 01:08:42 +0900

    open traq page

--- app/src/main/java/tech/trapti/q/MainActivity.kt
+++ app/src/main/java/tech/trapti/q/MainActivity.kt
@@ -2,11 +2,19 @@ package tech.trapti.q
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val webView = findViewById(R.id.webView) as WebView
+        webView.settings.databaseEnabled = true
+        webView.settings.javaScriptEnabled = true
+        webView.settings.setSupportMultipleWindows(true)
+        webView.settings.setSupportZoom(false)
+        webView.loadUrl("https://traq-dev.tokyotech.org")
     }
 }
