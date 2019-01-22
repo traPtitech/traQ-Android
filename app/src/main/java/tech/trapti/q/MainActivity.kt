b8a6d43d4ff4e0c79a59ac7b3d924bfec32faab2
tohutohu <tohu.soy@gmail.com>
2019-01-22 18:51:07 +0900

    キャッシュの導入 ＆ スクロールをスムーズにするためのハードウェアアクセラレーションの有効化

--- app/src/main/java/tech/trapti/q/MainActivity.kt
+++ app/src/main/java/tech/trapti/q/MainActivity.kt
@@ -4,14 +4,15 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.preference.PreferenceManager
+import android.support.customtabs.CustomTabsIntent
 import android.util.Log
-import android.webkit.JavascriptInterface
-import android.webkit.WebView
-import android.webkit.WebViewClient
+import android.webkit.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,16 +33,52 @@ class MainActivity : AppCompatActivity() {
         WebView.setWebContentsDebuggingEnabled(true)
 
         val webView = findViewById(R.id.webView) as WebView
+
+        webView.loadUrl("https://traq-dev.tokyotech.org")
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+                val host = request!!.url.host
+                Log.d("traq-debug", request.toString())
+                if (host.contains("traq-dev.tokyotech.org")) {
+                    return false
+                } else {
+                    val builder = CustomTabsIntent.Builder()
+                    builder.addDefaultShareMenuItem()
+
+                    val customTabsIntent = builder.build()
+
+                    customTabsIntent.launchUrl(applicationContext, request.url)
+                    return true
+                }
+            }
+
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                Log.d("traq-debug", url)
+                if (Uri.parse(url).host.contains("traq-dev.tokyotech.org")) {
+                    return false
+                } else {
+                    val builder = CustomTabsIntent.Builder()
+                    builder.addDefaultShareMenuItem()
+
+                    val customTabsIntent = builder.build()
+
+                    customTabsIntent.launchUrl(applicationContext, Uri.parse(url))
+                    return true
+                }
+            }
+        }
+
         webView.settings.databaseEnabled = true
         webView.settings.javaScriptEnabled = true
         webView.settings.setSupportMultipleWindows(true)
         webView.settings.setSupportZoom(false)
+
+        webView.settings.setAppCacheEnabled(true)
+        webView.settings.cacheMode = WebSettings.LOAD_CACHE_ELSE_NETWORK
+
         webView.settings.userAgentString += " traQ-Android"
 
-        webView.webViewClient = object: WebViewClient() {
-        }
-
-        webView.loadUrl("https://traq-dev.tokyotech.org")
 
         webView.addJavascriptInterface(Bridge(token), "Bridge")
     }
